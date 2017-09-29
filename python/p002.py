575c723fab2c2919e0653231ff8467b673e2725c
Dauzo <dauzo95@hotmail.com>
2017-09-29 22:38:37 +0200

    Create p002.py

---
+++ python/p002.py
@@ -0,0 +1,11 @@
+def fibonacci():
+    total = 0
+    a = 1
+    b = 2
+    while a <= 4000000:
+        if a % 2 == 0:
+            total += a
+        a, b = b, a + b
+    return total
+
+print(fibonacci())
